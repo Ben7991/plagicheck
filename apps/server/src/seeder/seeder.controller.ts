7da57bc127da108ec336da2bbdbe12528b0de98c
Bernard Teye <datssosh@gmail.com>
2025-01-25 17:30:01 +0000

    add createAdmin() route handler in SeederController and SeederService

--- apps/server/src/seeder/seeder.controller.ts
+++ apps/server/src/seeder/seeder.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { SeederService } from './seeder.service';
 
-@Controller('seeder')
-export class SeederController {}
+@Controller('seeders')
+@ApiTags('Seeders')
+export class SeederController {
+  constructor(private seederService: SeederService) {}
+
+  @Post('create-admin')
+  createAdmin() {
+    return this.seederService.createAdmin();
+  }
+}
--- apps/server/src/seeder/seeder.service.ts
+++ apps/server/src/seeder/seeder.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import * as bcryptjs from 'bcryptjs';
+
+import { UserEntity } from 'src/entities/user.entity';
 
 @Injectable()
-export class SeederService {}
+export class SeederService {
+  constructor(
+    private dataSource: DataSource,
+    private configService: ConfigService,
+  ) {}
+
+  async createAdmin() {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const adminDetails = this.configService.get<{
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        phone: string;
+      }>('admin');
+      const existingAdmin = await this.dataSource.manager.findOne(UserEntity, {
+        where: {
+          email: adminDetails?.email,
+        },
+      });
+
+      if (existingAdmin) {
+        throw new Error('Admin already created');
+      }
+
+      const saltRound = 10;
+      const hashedPassword = await bcryptjs.hash(
+        adminDetails!.password,
+        saltRound,
+      );
+
+      const admin = new UserEntity();
+      admin.id = adminDetails!.id;
+      admin.name = adminDetails!.name;
+      admin.email = adminDetails!.email;
+      admin.password = hashedPassword;
+      admin.phone = adminDetails!.phone;
+
+      await queryRunner.manager.save(admin);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      return { message: 'Admin created successfully' };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+      throw new BadRequestException((error as Error).message);
+    }
+  }
+}
