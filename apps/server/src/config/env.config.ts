c50ce291c30dd6d97ae05907eec3a6a0f1433402
Bernard Teye <datssosh@gmail.com>
2025-01-25 17:27:57 +0000

    add id and phone in admin data

--- apps/server/src/config/env.config.ts
+++ apps/server/src/config/env.config.ts
@@ -7,4 +7,11 @@ export default () => ({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
+  admin: {
+    id: process.env.ADMIN_ID,
+    name: process.env.ADMIN_NAME,
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+    phone: process.env.ADMIN_PHONE,
+  },
 });
