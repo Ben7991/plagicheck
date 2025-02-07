34e938d5c3e48268ee17b8977d5c938ab9efc8a7
Bernard Teye <datssosh@gmail.com>
2025-02-07 05:51:12 +0000

    add form validation logic and test for Login component

--- apps/client/src/pages/auth/login/Login.test.tsx
+++ apps/client/src/pages/auth/login/Login.test.tsx
@@ -1,14 +1,34 @@
-import { render, screen } from '@testing-library/react';
-// import { UserEvent } from '@testing-library/user-event';
-import '@testing-library/jest-dom';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { TextEncoder } from 'util';
+import '@testing-library/jest-dom';
 
 global.TextEncoder = TextEncoder;
 
-import Login from './Login';
 import { MemoryRouter } from 'react-router-dom';
+import Login from './Login';
+import { FormEvent } from 'react';
+
+afterEach(cleanup);
 
 describe('Login.tsx', () => {
+  let isCalled = false;
+  const mockHandleSubmit = jest
+    .fn()
+    .mockImplementation((event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const formData = new FormData(event.currentTarget);
+      const data = Object.fromEntries(formData);
+
+      if (data.username !== ' ' && data.password !== ' ') {
+        isCalled = true;
+      }
+    });
+
+  beforeEach(() => (isCalled = false));
+
   it('should render Login component', () => {
     render(
       <MemoryRouter>
@@ -19,5 +39,72 @@ describe('Login.tsx', () => {
       name: /login/i,
     });
     expect(appNameElement).toBeInTheDocument();
+  });
+
+  it('should ensure that the password can be viewed when the eye button is clicked', async () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>,
+    );
+    const eyeButton = container.querySelector(
+      'div > form > div:nth-child(2) > div > button',
+    )!;
+    await userEvent.click(eyeButton);
+    const passwordInput = screen.getByPlaceholderText(
+      /type your password/i,
+    ) as HTMLInputElement;
+
+    expect(passwordInput.type).toBe('text');
+  });
+
+  it('should not submit the form when the input fields are empty', async () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>,
+    );
+    const emailInput = screen.getByPlaceholderText(/your email/i);
+    await userEvent.click(emailInput);
+    await userEvent.keyboard(' ');
+
+    const passwordInput = screen.getByPlaceholderText(/type your password/i);
+    await userEvent.click(passwordInput);
+    await userEvent.keyboard(' ');
+
+    const submitButton = screen.getByRole('button', {
+      name: /login/i,
+    });
+    const formElement = screen.getByRole('login-form');
+    formElement.onsubmit = mockHandleSubmit;
+
+    await userEvent.click(submitButton);
+    expect(isCalled).toBeFalsy();
+  });
+
+  it('should submit the form when the input fields are not empty', async () => {
+    render(
+      <MemoryRouter>
+        <Login />
+      </MemoryRouter>,
+    );
+    const emailInput = screen.getByPlaceholderText(/your email/i);
+    await userEvent.click(emailInput);
+    await userEvent.keyboard('test@gmail.com');
+
+    const passwordInput = screen.getByPlaceholderText(/type your password/i);
+    await userEvent.click(passwordInput);
+    await userEvent.keyboard('test123@');
+
+    const submitButton = screen.getByRole('button', {
+      name: /login/i,
+    });
+
+    const form = screen.getByRole('login-form');
+    form.onsubmit = mockHandleSubmit;
+
+    await userEvent.click(submitButton);
+
+    expect(isCalled).toBeTruthy();
   });
 });
--- apps/client/src/pages/auth/login/Login.tsx
+++ apps/client/src/pages/auth/login/Login.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
+import { schema } from './login.util';
+import { Inputs } from './login.util';
 import FormControl from '../../../components/atoms/form-elements/form-control/FormControl';
 import FormGroup from '../../../components/atoms/form-elements/form-group/FormGroup';
 import Label from '../../../components/atoms/form-elements/label/Label';
@@ -10,9 +14,22 @@ import AuthDescriptor from '../../../components/molecules/auth-descriptor/AuthDe
 import FormFooter from '../../../components/atoms/form-elements/form-footer/FormFooter';
 import Button from '../../../components/atoms/button/Button';
 import PasswordToggler from '../../../components/molecules/password-toggler/PasswordToggler';
+import FormControlError from '../../../components/atoms/form-elements/form-control-error/FormControlError';
 
 export default function Login(): JSX.Element {
   const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -20,21 +37,30 @@ export default function Login(): JSX.Element {
         title="Login"
         info="Please enter your login details below to access your account"
       />
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)} role="login-form">
         <FormGroup className="mb-4">
-          <Label>Email/Staff ID</Label>
+          <Label htmlFor="username">Email / ID</Label>
           <FormControl
             leftIcon={<EmailIcon />}
             type="text"
             placeholder="Your email or id"
+            id="username"
+            {...register('username')}
+            hasError={!!errors.username}
           />
+          {errors.username && (
+            <FormControlError message={errors.username.message} />
+          )}
         </FormGroup>
         <FormGroup className="mb-4">
-          <Label>Password</Label>
+          <Label htmlFor="password">Password</Label>
           <FormControl
             leftIcon={<PasswordIcon />}
             type={showPassword ? 'text' : 'password'}
             placeholder="Type your password here"
+            id="password"
+            {...register('password')}
+            hasError={!!errors.password}
             rightIcon={
               <PasswordToggler
                 state={showPassword}
@@ -42,6 +68,9 @@ export default function Login(): JSX.Element {
               />
             }
           />
+          {errors.password && (
+            <FormControlError message={errors.password.message} />
+          )}
         </FormGroup>
         <div className="flex justify-between mb-4">
           <label className="flex gap-2 items-center select-none">
@@ -55,7 +84,7 @@ export default function Login(): JSX.Element {
           </Link>
         </div>
         <FormFooter className="flex-col">
-          <Button el="button" variant="primary" type="button">
+          <Button el="button" variant="primary" type="submit">
             Login
           </Button>
         </FormFooter>
