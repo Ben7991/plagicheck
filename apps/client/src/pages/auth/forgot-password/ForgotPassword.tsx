967693e59b4676f2ef3a7ff6068029c3b5a5511d
Bernard Teye <datssosh@gmail.com>
2025-02-15 08:44:29 +0000

    add request password reset login in ForgotPassword component

--- apps/client/src/pages/auth/forgot-password/ForgotPassword.tsx
+++ apps/client/src/pages/auth/forgot-password/ForgotPassword.tsx
@@ -1,7 +1,7 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import { schema, Inputs } from './forgot-password.util';
+import { schema, Inputs, requestPasswordReset } from './forgot-password.util';
 import Button from '../../../components/atoms/button/Button';
 import FormControl from '../../../components/atoms/form-elements/form-control/FormControl';
 import FormFooter from '../../../components/atoms/form-elements/form-footer/FormFooter';
@@ -11,23 +11,55 @@ import EmailIcon from '../../../components/atoms/icons/EmailIcon';
 import LeftArrowIcon from '../../../components/atoms/icons/LeftArrowIcon';
 import AuthDescriptor from '../../../components/molecules/auth-descriptor/AuthDescriptor';
 import FormControlError from '../../../components/atoms/form-elements/form-control-error/FormControlError';
+import { useAlert } from '../../../util/hooks/use-alert/useAlert';
+import { useRef, useState } from 'react';
+import { AlertVariant } from '../../../util/enum/alert-variant.enum';
+import Alert from '../../../components/molecules/alert/Alert';
 
 export default function ForgotPassword(): JSX.Element {
+  const timerRef = useRef<NodeJS.Timeout>();
+  const [isLoading, setIsLoading] = useState(false);
+  const { alertState, alertInfo, hideAlert, setAlertInfo, showAlert } =
+    useAlert();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<Inputs>({
     mode: 'onBlur',
     resolver: yupResolver(schema),
   });
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    setIsLoading(true);
+
+    const result = await requestPasswordReset(data);
+    setAlertInfo({
+      message: result.message,
+      variant: AlertVariant.SUCCESS,
+    });
+
+    timerRef.current = setTimeout(() => {
+      showAlert();
+      setIsLoading(false);
+      reset();
+    }, 2000);
   };
 
   return (
     <>
+      {alertState && alertInfo && (
+        <Alert
+          message={alertInfo.message}
+          onHide={hideAlert}
+          variant={alertInfo.variant}
+        />
+      )}
       <AuthDescriptor
         title="Forgot Password"
         info="Please enter your email to receive the reset link in your mail."
@@ -46,8 +78,13 @@ export default function ForgotPassword(): JSX.Element {
           {errors.email && <FormControlError message={errors.email.message} />}
         </FormGroup>
         <FormFooter className="flex-col gap-4">
-          <Button el="button" variant="primary" type="button">
-            Send reset link
+          <Button
+            el="button"
+            variant="primary"
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? 'Loading...' : 'Send reset link'}
           </Button>
           <Button
             el="link"
--- apps/client/src/pages/auth/forgot-password/forgot-password.util.ts
+++ apps/client/src/pages/auth/forgot-password/forgot-password.util.ts
@@ -10,3 +10,20 @@ export const schema = yup.object({
     .required('Email is required')
     .matches(/^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email address'),
 });
+
+export async function requestPasswordReset(data: { email: string }) {
+  const response = await fetch(
+    'http://localhost:3000/api/auth/request-password-reset',
+    {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  const result = (await response.json()) as { message: string };
+
+  return result;
+}
