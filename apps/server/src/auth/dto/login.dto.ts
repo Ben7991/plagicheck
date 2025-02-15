b5691ae9563d66d575dcd6f6373a8b64b408edf2
Bernard Teye <datssosh@gmail.com>
2025-02-15 06:59:16 +0000

    refactor login.dto.ts

--- apps/server/src/auth/dto/login.dto.ts
+++ apps/server/src/auth/dto/login.dto.ts
@@ -2,12 +2,10 @@ import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty()
   @ApiProperty()
   username: string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsNotEmpty()
   @ApiProperty()
   password: string;
