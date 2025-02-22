ff9457f50d6abf9d8e63ef8fbba76867bdd0aecf
Bernard Teye <datssosh@gmail.com>
2025-02-22 12:17:08 +0000

    add active status in UserProfile component

--- apps/client/src/components/molecules/user-profile/UserProfile.tsx
+++ apps/client/src/components/molecules/user-profile/UserProfile.tsx
@@ -5,8 +5,9 @@ import { UserProfileProps } from './user-profile.util';
 export default function UserProfile({ className }: UserProfileProps) {
   return (
     <div className={`flex gap-4 items-center ${className}`}>
-      <div className="w-12 h-12 rounded-full border border-[var(--gray-700)] flex items-center justify-center">
+      <div className="w-12 h-12 rounded-full border border-[var(--gray-700)] flex items-center justify-center relative">
         <UserIcon />
+        <span className="w-4 h-4 border border-white rounded-full bg-[var(--success-100)] absolute bottom-0 -right-1"></span>
       </div>
       <div className="space-y-[2px]">
         <Headline type="h5">Esther Howard</Headline>
