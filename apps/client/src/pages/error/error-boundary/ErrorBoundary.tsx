9909eb3e260510d924c12f497a924577bae53338
Bernard Teye <datssosh@gmail.com>
2025-02-22 13:51:13 +0000

    refactor ErrorBoundary component

--- apps/client/src/pages/error/error-boundary/ErrorBoundary.tsx
+++ apps/client/src/pages/error/error-boundary/ErrorBoundary.tsx
@@ -6,27 +6,32 @@ import LeftArrowIcon from '../../../components/atoms/icons/LeftArrowIcon';
 type ErrorBoundaryProps = {
   message: string;
   path?: string;
+  fullScreen?: boolean;
 };
 
-export default function ErrorBoundary({ message, path }: ErrorBoundaryProps) {
+export default function ErrorBoundary({
+  message,
+  path,
+  fullScreen = true,
+}: ErrorBoundaryProps) {
   return (
-    <main className="w-full h-screen bg-[var(--gray-100)] fixed top-0 left-0 flex items-center justify-center">
+    <main
+      className={`w-full h-screen bg-[var(--gray-100)] ${fullScreen && 'fixed top-0 left-0'} flex items-center justify-center`}
+    >
       <section className="text-center space-y-4">
-        <AppLogo />
+        <AppLogo className="flex justify-center" />
         <Headline type="h3">Oops!!!</Headline>
         <p>{message}</p>
-        {path && (
-          <div>
-            <Button
-              el="link"
-              variant="secondary"
-              className="flex items-center justify-center gap-2"
-              href="/"
-            >
-              <LeftArrowIcon /> Back to login
-            </Button>
-          </div>
-        )}
+        <div className="mx-auto w-48">
+          <Button
+            el="link"
+            variant="secondary"
+            className="flex items-center justify-center gap-2"
+            href={path}
+          >
+            <LeftArrowIcon /> Back to login
+          </Button>
+        </div>
       </section>
     </main>
   );
