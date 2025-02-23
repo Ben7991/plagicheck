9b9b40bab069d02480cb84c9593695f292d87125
Bernard Teye <datssosh@gmail.com>
2025-02-23 20:22:07 +0000

    add logout() in side-drawer.util.ts function and logout form functionality in SideDrawer component

--- apps/client/src/components/organisms/side-drawer/SideDrawer.tsx
+++ apps/client/src/components/organisms/side-drawer/SideDrawer.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { GoDatabase } from 'react-icons/go';
 import { RxCounterClockwiseClock } from 'react-icons/rx';
 
-import { NavLinkClassNameProps, SideDrawerProps } from './side-drawer.util';
+import {
+  logout,
+  NavLinkClassNameProps,
+  SideDrawerProps,
+} from './side-drawer.util';
+import { useAlert } from '../../../util/hooks/use-alert/useAlert';
+import { AlertVariant } from '../../../util/enum/alert-variant.enum';
+import { useAppDispatch } from '../../../store/store.util';
+import { logoutAuthUser } from '../../../store/slice/auth/auth.slice';
 import AppLogo from '../../molecules/app-logo/AppLogo';
 import DashboardIcon from '../../atoms/icons/DashboardIcon';
 import PlagiCheckIcon from '../../atoms/icons/PlagiCheckIcon';
@@ -17,10 +25,35 @@ import UserProfile from '../../molecules/user-profile/UserProfile';
 import Modal from '../modal/Modal';
 import FormFooter from '../../atoms/form-elements/form-footer/FormFooter';
 import Button from '../../atoms/button/Button';
+import Alert from '../../molecules/alert/Alert';
 
 export default function SideDrawer({ show, onHide }: SideDrawerProps) {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
   const [showDropDown, setShowDropDown] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { alertState, alertInfo, hideAlert, setAlertInfo, showAlert } =
+    useAlert();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await logout();
+      dispatch(logoutAuthUser());
+      navigate('/');
+    } catch (error) {
+      setAlertInfo({
+        message: (error as Error).message,
+        variant: AlertVariant.ERROR,
+      });
+      showAlert();
+    }
+
+    setIsLoading(false);
+  };
 
   const navLinkClass = ({ isActive, isPending }: NavLinkClassNameProps) => {
     const className = 'flex items-center gap-2 py-2 px-4 rounded-lg';
@@ -123,6 +156,7 @@ export default function SideDrawer({ show, onHide }: SideDrawerProps) {
         <div className="px-[25px]">
           <button
             className="flex items-center gap-2 py-2 px-4 rounded-lg hover:bg-[var(--gray-100)] cursor-pointer w-full"
+            type="button"
             onClick={() => {
               setShowLogoutModal(true);
               onHide();
@@ -139,7 +173,7 @@ export default function SideDrawer({ show, onHide }: SideDrawerProps) {
             Are you sure you want to logout? By logging out, you will be
             securely logged out of the system and your session will be ended.
           </p>
-          <form>
+          <form onSubmit={handleSubmit}>
             <FormFooter className="gap-[19px]">
               <Button
                 el="button"
@@ -155,12 +189,21 @@ export default function SideDrawer({ show, onHide }: SideDrawerProps) {
                 variant="danger"
                 type="submit"
                 className="basis-[197px]"
+                disabled={isLoading}
               >
-                Yes, logout
+                {isLoading ? 'Loading...' : 'Yes, logout'}
               </Button>
             </FormFooter>
           </form>
         </Modal>
+      )}
+
+      {alertState && alertInfo && (
+        <Alert
+          message={alertInfo.message}
+          variant={alertInfo.variant}
+          onHide={hideAlert}
+        />
       )}
     </>
   );
--- apps/client/src/components/organisms/side-drawer/side-drawer.util.ts
+++ apps/client/src/components/organisms/side-drawer/side-drawer.util.ts
@@ -7,3 +7,17 @@ export type SideDrawerProps = {
   show: boolean;
   onHide: VoidFunction;
 };
+
+export async function logout() {
+  const response = await fetch('http://localhost:3000/api/auth/logout', {
+    method: 'POST',
+    credentials: 'include',
+  });
+  await response.json();
+
+  if (!response.ok) {
+    throw new Error('Something went wrong');
+  }
+
+  return;
+}
