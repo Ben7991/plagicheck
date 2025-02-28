a29d0c15fa3e842c09ffc35c3e96a9b2627f5750
Bernard Teye <datssosh@gmail.com>
2025-02-28 07:29:16 +0000

    refactor useAppSelector hook in store.util.ts

--- apps/client/src/store/store.util.ts
+++ apps/client/src/store/store.util.ts
@@ -3,4 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { store } from './store';
 
 export const useAppDispatch = useDispatch.withTypes<typeof store.dispatch>();
-export const useAppSelector = useSelector.withTypes<typeof store.getState>();
+
+type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector = useSelector.withTypes<RootState>();
