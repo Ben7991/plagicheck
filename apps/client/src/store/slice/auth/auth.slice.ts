79c74c0e034de0279e1d728d9fc414b118c33f1b
Bernard Teye <datssosh@gmail.com>
2025-02-28 07:30:11 +0000

    add changeEmailandName reducer function in auth.slice.ts

--- apps/client/src/store/slice/auth/auth.slice.ts
+++ apps/client/src/store/slice/auth/auth.slice.ts
@@ -19,8 +19,20 @@ const authSlice = createSlice({
       state.data = undefined;
       state.isAuthenticated = false;
     },
+    changeEmailandName: (
+      state,
+      action: PayloadAction<Pick<AuthData, 'email' | 'name'>>,
+    ) => {
+      if (!state.data) {
+        return;
+      }
+
+      state.data.name = action.payload.name;
+      state.data.email = action.payload.email;
+    },
   },
 });
 
 export default authSlice.reducer;
-export const { setAuthUser, logoutAuthUser } = authSlice.actions;
+export const { setAuthUser, logoutAuthUser, changeEmailandName } =
+  authSlice.actions;
