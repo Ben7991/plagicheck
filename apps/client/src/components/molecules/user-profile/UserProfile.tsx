20c08c6d02b1d35c3a00750060c9811acff869ac
Bernard Teye <datssosh@gmail.com>
2025-02-28 07:06:56 +0000

    refactor UserProfile component to use authenticated user data

--- apps/client/src/components/molecules/user-profile/UserProfile.tsx
+++ apps/client/src/components/molecules/user-profile/UserProfile.tsx
@@ -1,8 +1,11 @@
 import Headline from '../../atoms/headline/Headline';
 import UserIcon from '../../atoms/icons/UserIcon';
 import { UserProfileProps } from './user-profile.util';
+import { useAppSelector } from '../../../store/store.util';
 
 export default function UserProfile({ className }: UserProfileProps) {
+  const authUser = useAppSelector((state) => state.auth.data);
+
   return (
     <div className={`flex gap-4 items-center ${className}`}>
       <div className="w-12 h-12 rounded-full border border-[var(--gray-700)] flex items-center justify-center relative">
@@ -10,8 +13,8 @@ export default function UserProfile({ className }: UserProfileProps) {
         <span className="w-4 h-4 border border-white rounded-full bg-[var(--success-100)] absolute bottom-0 -right-1"></span>
       </div>
       <div className="space-y-[2px]">
-        <Headline type="h5">Esther Howard</Headline>
-        <p className="text-[0.875em]">estherhoward@gmail.com</p>
+        <Headline type="h5">{authUser?.name}</Headline>
+        <p className="text-[0.875em]">{authUser?.email}</p>
       </div>
     </div>
   );
