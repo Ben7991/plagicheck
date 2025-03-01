99e5d2400abc047ef961d06335461985d5a81e15
Bernard Teye <datssosh@gmail.com>
2025-03-01 10:40:51 +0000

    add SubPageHeader component to AcademicDivision component

--- apps/client/src/pages/dashboard/academic-division/AcademicDivision.tsx
+++ apps/client/src/pages/dashboard/academic-division/AcademicDivision.tsx
@@ -1,10 +1,24 @@
+import Button from '../../../components/atoms/button/Button';
 import PageHeader from '../../../components/organisms/page-header/PageHeader';
+import SubPageHeader from '../../../components/organisms/page-header/SubPageHeader';
 
 export default function AcademicDivision() {
   return (
     <>
       <PageHeader />
-      <h1>AcademicDivision</h1>
+      <SubPageHeader
+        title="Academic Unit"
+        description="Add faculties and departments here"
+      >
+        <div className="flex gap-4">
+          <Button el="button" variant="secondary" className="w-[162px]">
+            Add Department
+          </Button>
+          <Button el="button" variant="primary" className="w-[128px]">
+            Add Faculty
+          </Button>
+        </div>
+      </SubPageHeader>
     </>
   );
 }
