0bf1b70f45d4915e13c38e026f26fc1e91eb698e
Bernard Teye <datssosh@gmail.com>
2025-04-12 05:35:25 +0000

    add ExportIcon component

---
+++ apps/client/src/components/atoms/icons/ExportIcon.tsx
@@ -0,0 +1,33 @@
+export default function ExportIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13 11L21.2 2.79999"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21.9992 6.8V2H17.1992"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22H15C20 22 22 20 22 15V13"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
